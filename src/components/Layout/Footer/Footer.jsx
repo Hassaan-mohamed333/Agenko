831909eacb498638b9170b5220dc6b83ac50d298
hassaan <hassaaanmohamed333@gmail.com>
2025-10-05 13:52:58 +0300

    first commit8

--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer() {
       <div className="shape shape-one absolute left-0 bottom-24 z-0">
         <span>
           <img
-            src="/src/assets/images/element1.png"
+            src="@/src/assets/images/element1.png"
             alt="Shape"
             className={style.rotate360}  
           />
@@ -17,7 +17,7 @@ export default function Footer() {
       <div className="shape shape-two absolute right-0 bottom-36 z-[0]">
         <span>
           <img
-            src="/src/assets/images/element2.png"
+            src="@/src/assets/images/element2.png"
             alt="Shape"
             className={style.rotate360}  
           />
